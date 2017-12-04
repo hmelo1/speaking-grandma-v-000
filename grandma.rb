97780caed843766e011997d380d3804e8a9217bd
Eddie Melo <heriberto@melo.nyc>
2017-12-04 03:36:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -21,6 +21,8 @@ def speak_to_grandma(phrase)
     #return "HUH?! SPEAK UP, SONNY!"
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+    return "I LOVE YOU GRANDMA!"
   else
     return "NO, NOT SINCE 1938!"
   end
