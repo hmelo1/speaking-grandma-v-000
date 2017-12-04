ffb040f0e311e3253b99988ccb35b2fa5ee0a7c5
Eddie Melo <heriberto@melo.nyc>
2017-12-04 03:35:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,13 +13,15 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  #if phrase == "I LOVE YOU GRANDMA!"
+    #return "I LOVE YOU TOO PUMPKIN!"
+  #elsif phrase == phrase.upcase
+    #return "NO, NOT SINCE 1938!"
+  #elsif phrase == phrase.capitalize
+    #return "HUH?! SPEAK UP, SONNY!"
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
     return "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.capitalize
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
   end
 end
